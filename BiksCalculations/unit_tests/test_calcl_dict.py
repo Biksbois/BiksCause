2bde1fd20f3b5f84721a1b44c8673e745d415cf9
unknown <jeppe@bandsholm.com>
2021-04-27 10:33:12 +0200

    UnTested unitest

---
+++ BiksCalculations/unit_tests/test_calcl_dict.py
@@ -0,0 +1,31 @@
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+import unittest
+import datetime
+import optimizer
+from dataset_object import *
+from LL import calc_lambda
+from NST import *
+from CIR import *
+
+_ds_path = "BiksCalculations/csv/data.csv"
+
+class test_dict(unittest.TestCase):
+    def test_calc_dict_nec_win3(self):
+        dst_obj = init_obj_test()
+        window_size = 3
+        expected = {'x':{'x':1 ,'y':4, 'z':4}, 'y':{'x':4, 'y':5 ,'z': 6}, 'z':{'x':4, 'y':4, 'z':3}}
+        actual = generate_nec(dst_obj)
+        self.assertDictEqual(expected,actual)
+        
+    def test_calc_dict_suf_win3(self):
+        window_size = 3
+        expected = {'x':{'x':1 'y': 3, 'z':4}, 'y':{'x':5 'y':4 'z':6}, 'z':{'x':4 'y':5 'z':2 }}
+        actual = yeet.placeholder()
+        self.assertDictEqual(expected,actual)
+        
+        
+        
