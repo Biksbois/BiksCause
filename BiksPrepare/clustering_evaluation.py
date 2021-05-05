61fd5319136fc86cf193401dd9807beb3636f53d
Bjarke Baltzersen <44190630+Bbaltzersen@users.noreply.github.com>
2021-05-05 12:53:05 +0200

    clustering_evaluation, not yet tested, but should work.

--- BiksPrepare/clustering_evaluation.py
+++ BiksPrepare/clustering_evaluation.py
@@ -1,7 +1,6 @@
 import pandas as pd
+import csv
 import os
-
-
 
 def scan_directory(path):
     csv_paths = []
@@ -32,50 +31,51 @@ def check_lower(duration, cur_lower):
 def check_upper(duration, cur_upper):
     return duration > cur_upper
 
-def add_upper_lower_cl_arr(cl_arr):
-    new_cl_arr = []
-    lower_upper = [0, 0]
-    for cl in cl_arr:
-        new_cl_arr.append((cl, lower_upper))
-    return new_cl_arr
-
-def iniate_check(ul_bound, dur):
-    if cl[1][0] == 0:
-        cl[1][0] = row[dur_col]
-    if cl[1][1] == 0:
-        cl[1][1] = row[dur_col]
-    if check_lower(row[dur_col], cl[1][0]):
-        cl[1][0] = row[dur_col]
-    if check_upper(row[dur_col], cl[1][1]):
-        cl[1][1] = row[dur_col]
-
-
 def find_cluster_durations(df, cl_arr, dur_col):
-    cl_ul_arr = add_upper_lower_cl_arr(cl_arr)
-    event_arr = {}
+    cluster_dict = {}
+    count_clusters = 0
     for index, row in df.iterrows():
         for cl in cl_arr:
-            print(row[cl[0]])
-            # if row[cl[0]] not in event_dict:
-            #     event_dict[row[cl[0]]] = [0, 0]
-            # else:
-            #     if check_lower(row[dur_col], event_dict[cl[0]][0]):
-            #         event_dict[cl[0]][0] = row[dur_col]
-            #     if check_upper(row[dur_col], event_dict[cl[0]][1]):
-            #         event_dict[cl[0]][1] = row[dur_col]
+            if row[cl] not in cluster_dict:
+                cluster_dict[row[cl]] = [row[dur_col], row[dur_col], 1]
+            else:
+                if check_lower(row[dur_col], cluster_dict[row[cl]][0]):
+                    cluster_dict[row[cl]][0] = row[dur_col]
+                if check_upper(row[dur_col], cluster_dict[row[cl]][1]):
+                    cluster_dict[row[cl]][1] = row[dur_col]
+                cluster_dict[row[cl]][2] += 1
+    return cluster_dict
 
 def evaluate_clusters_in_event(df_data_arr, dur_col):
+    event_dict = {}
     for data in df_data_arr:
-        find_cluster_durations(data[1], data[2], dur_col)
-        #find_cluster_durations(df, dur_col, cl_arr)
+        if data[0] not in event_dict:
+            event_dict[data[0]] = find_cluster_durations(data[1], data[2], dur_col)
+        else:
+            event_dict[data[0]].update(find_cluster_durations(data[1], data[2], dur_col))
+    return event_dict
 
-def create_cluster_files():
-    pass
+def convert_dict_to_string(event_dict):
+    with open('test.csv', 'w', newline='') as csvfile:
+        csv_columns = ['Event','Cluster', 'Interval_Start', 'Interval_End', 'Amount', 'GVF']
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for event, cluster_arr in event_dict.items():
+            writer.writerow({'Event': event, 'Cluster': '', 'Interval_Start': '', 'Interval_End': '','Amount': '', 'GVF': ''})
+            for cluster, interval in cluster_arr.items():
+                print(interval)
+                writer.writerow({'Event': '', 'Cluster': cluster, 'Interval_Start': interval[0], 'Interval_End': interval[1],'Amount': interval[2], 'GVF': ''})
 
-def run_evaluation(path, dur_col = 'duration', cl_col = 'cluster'):
-    csv_paths = scan_directory(path)
+def create_cluster_files(event_dict, output_path):
+    text = open(output_path, 'w')
+    clust_text = convert_dict_to_string(event_dict)
+    text.close()
+
+def run_evaluation(input_path, output_path = 'temp.txt', dur_col = 'duration', cl_col = 'cluster'):
+    csv_paths = scan_directory(input_path)
     df_data_arr = process_csv_files(csv_paths, cl_col)
-    event_arr = evaluate_clusters_in_event(df_data_arr, dur_col)
+    event_dict = evaluate_clusters_in_event(df_data_arr, dur_col)
+    create_cluster_files(event_dict, output_path)
 
 if __name__ == '__main__':
     run_evaluation(r'BiksCalculations\csv\temp_csv')
